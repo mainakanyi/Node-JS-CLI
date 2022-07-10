50937e9e57f77b5549f19c156bde8cd646188d04
mainakanyi <mainakanyi9@gmail.com>
2022-07-10 20:14:10 +0300

    James Kanyi Profile with Nodejs CLI

---
+++ bin/index.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+console.log(`Name: James Kanyi Maina
+Website: https://keydigital.technology
+Github: https://github.com/mainakanyi
+LinkedIn: https://www.linkedin.com/in/james-kanyi-06010872/`);
+
